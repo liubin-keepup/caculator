64974d8840a57ee8992c5a5c0d5adbd63250308b
liubin-keepup <837479017@qq.com>
2020-08-24 20:08:20 +0800

    init

---
+++ git连接仓库指南.md
@@ -0,0 +1,4 @@
+## git绑定仓库操作步骤
+1.git config --global user.name '账户名'  ///账户名 最好用git相关的邮箱
+2.git config --global user.email '邮箱'  //可以和用户名一致但是要是邮箱格式
+2. ssh-keygen -t rsa -C "邮箱" //最好和第二步 邮箱一致
---
+++ src/components/Calculator.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="calcul">
+    <div class="calc_methods">
+      <div class="front">
+        <el-input v-model="resault" class="show_res" number></el-input>
+      </div>
+      <div>
+        <div class="calc_num">
+          <span v-for="(n,i) in nums" :key="i" @click="getRes(n)">{{n}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  data() {
+    return {
+      nums: ["清空", "+", "-", "×", 1, 2, 3, "÷", 4, 5, 6, '删除',7, 8, 9, 0, "="],
+      join_nums:[],
+      
+      way:'',
+      resault: "",
+    };
+  },
+  methods: {
+    getRes(n) {
+        let type =typeof(n)
+        
+        if(type="number"){
+            this.resault+=n
+        }else if(n=='string'){
+            this.resault.indexOf()
+        }
+    }
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+#calcul {
+  width: 400px;
+  height: 400px;
+  border: 1px solid blue;
+  margin: 0 auto;
+  padding: 20px;
+}
+.show_res .el-input__inner {
+  text-align: end;
+}
+.calc_num span {
+  display: inline-block;
+  width: 12%;
+  border: 1px solid;
+  padding: 5px;
+  margin: 15px;
+  border-radius: 14%;
+}
+</style>
